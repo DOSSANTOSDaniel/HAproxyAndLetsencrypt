cdabca8f89e25655ecd3a30c31250ceefe3f8ceb
DOS SANTOS <danielitto91@gmail.com>
2020-02-19 15:55:08 +0100

    Update RenewWithCron.sh

--- RenewWithCron.sh
+++ RenewWithCron.sh
@@ -14,18 +14,21 @@
 #Authors     :  daniel.massy@gmail.com, pouteau.aurelie14@gmail.com
 #            :  yann.ndongui@gmail.com, killian.boulard@gmail.com                                                 
 ###----------------------------------------------------------------------------------------------------###
-
+# Exit si erreur
 set -e
 
+# Variables
 declare -r FullKeyDomaine="web.tpdaniel.fr.pem"
 declare -r HaproxyCrtDir="/etc/haproxy/certs"
 declare Rep="$(ls -d /etc/letsencrypt/live/*[!README])"
 
+# concaténation
 for Dir in "${Rep}"
 do
   bash -c "cat ${Dir}/privkey.pem ${Dir}/fullchain.pem > ${HaproxyCrtDir}/${FullKeyDomaine}"
   if [ "${?}" == "0" ]
   then
+    # Logger permet de créer des logs sur /var/log/syslog
     /usr/bin/logger -t HaproxyRenew "Mise à jour du fichier ${HaproxyCrtDir}/${FullKeyDomaine} réussi !"
   else
     /usr/bin/logger -t HaproxyRenew "Erreur de concaténation du certificat !"
@@ -33,6 +36,8 @@ do
   fi
 done
 
+# Protéction des certificats
 /usr/bin/chmod -R go-rwx ${HaproxyCrtDir}
 
+# Recharger la configuration de HAproxy
 /etc/init.d/haproxy reload | /usr/bin/logger -t HaproxyRenew
