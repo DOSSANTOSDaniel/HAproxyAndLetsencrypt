1d2596d080805d676d0f6df48513bc1170bdc587
DOS SANTOS <danielitto91@gmail.com>
2020-02-19 16:14:04 +0100

    Update LetsencryptRenew.sh

--- LetsencryptRenew.sh
+++ LetsencryptRenew.sh
@@ -1,26 +1,39 @@
 #!/bin/bash
 
 ###----------------------------------------------------------------------------------------------------###
-#Usage       :                                                                                              
+#Usage       :  30 19 20 */2 * root /usr/bin/bash /opt/certbot/LetsencryptRenew.sh                                                                                          
 #Description :  Permet d’automatiser le renouvellement de certificats Let’s Encrypt.                                                                               
 #            :  A exécuter en root avec cron.                                                                                         
 #Authors     :  daniel.massy@gmail.com, pouteau.aurelie14@gmail.com
 #            :  yann.ndongui@gmail.com, killian.boulard@gmail.com                                                 
 ###----------------------------------------------------------------------------------------------------###
 
+# Exit si erreur
 set -e
 
+# Variables
 declare -r FullKeyDomaine="web.tpdaniel.fr.pem"
 declare -r HaproxyCrtDir="/etc/haproxy/certs"
-declare Rep=$(ls -d /etc/letsencrypt/live/*[!README])
+declare Rep="$(ls -d /etc/letsencrypt/live/*[!README])"
 
 /usr/bin/certbot -q renew --force-renewal | /usr/bin/logger -t HaproxyRenew
 
-for Dir in $Rep
+# concaténation
+for Dir in "${Rep}"
 do
-  bash -c "cat $Dir/privkey.pem $Dir/fullchain.pem > ${HaproxyCrtDir}/${FullKeyDomaine}"
+  bash -c "cat ${Dir}/privkey.pem ${Dir}/fullchain.pem > ${HaproxyCrtDir}/${FullKeyDomaine}"
+  if [ "${?}" == "0" ]
+  then
+    # Logger permet de créer des logs sur /var/log/syslog
+    /usr/bin/logger -t HaproxyRenew "Mise à jour du fichier ${HaproxyCrtDir}/${FullKeyDomaine} réussi !"
+  else
+    /usr/bin/logger -t HaproxyRenew "Erreur de concaténation du certificat !"
+    exit 0
+  fi
 done
 
+# Protéction des certificats
 /usr/bin/chmod -R go-rwx ${HaproxyCrtDir}
 
+# Recharger la configuration de HAproxy
 /etc/init.d/haproxy reload | /usr/bin/logger -t HaproxyRenew
