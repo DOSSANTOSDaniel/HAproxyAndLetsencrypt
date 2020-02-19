649c237d2284f7bd2c423ba894efc4a208bcc4aa
DOS SANTOS <danielitto91@gmail.com>
2020-02-19 15:22:33 +0100

    Update RenewWithCron.sh

--- RenewWithCron.sh
+++ RenewWithCron.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
 ###----------------------------------------------------------------------------------------------------###
-#Usage       :  /usr/bin/certbot renew --renew-hook 'bash RenewWithCron.sh'
-#            :  
+#Usage       :  30 19 * * 5 /usr/bin/certbot renew --renew-hook 'bash RenewWithCron.sh'
+#            :  Ce script sera lancé tous les vendredi à 19h:30.
+#            :  L'option renew permet de vérifier s'il reste moins de 30 jour avant l'expiration du certificat,
+#            :  s'il reste moins de 30 jours alors le hook est exécuté, ici le script RenewWithCron.sh
 #            :  Consultation des logs: cat /var/log/syslog | grep "HaproxyRenew"
+#            :
 #Description :  Permet d’automatiser le renouvellement de certificats Let’s Encrypt.                                                                               
-#            :  A exécuter en root avec cron.                                                                                         
+#            :  A exécuter en root avec cron.
+#            :
 #Authors     :  daniel.massy@gmail.com, pouteau.aurelie14@gmail.com
 #            :  yann.ndongui@gmail.com, killian.boulard@gmail.com                                                 
 ###----------------------------------------------------------------------------------------------------###
